80fc3105c86dfd611f2f55beb4541f5f2335130b
Samir <saamalam@gmail.com>
2019-07-02 12:48:42 +0545

    minor changes

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -6,7 +6,7 @@ export default [
     exact: true,
     component: () => import('../components/splash'),
     seo: {
-      title: 'NewsAPI ReactPWA: A developer friendly ReactJS boilerplate with NewsAPI| NewsAPI ReactPWA Demo',
+      title: 'NewsAPI ReactPWA: A developer friendly ReactJS boilerplate with NewsAPI | NewsAPI ReactPWA Demo',
       description: 'Create Upgradable, SEO friendly Progressive web applications with ReactPWA. Its fast and developer friendly and more importantly its UPGRADABLE!',
       image: SplashImage,
     },
