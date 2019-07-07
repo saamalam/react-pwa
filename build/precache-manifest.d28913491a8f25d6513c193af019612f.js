cfec71b1e30973b5c521951b2040a768397c90b6
Samir <saamalam@gmail.com>
2019-07-08 01:39:24 +0545

    Updates

---
+++ build/precache-manifest.d28913491a8f25d6513c193af019612f.js
@@ -0,0 +1,131 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/css/456767719c2ad7a9cabd.css"
+  },
+  {
+    "url": "/css/4c5548733f4bec0f19d3.css"
+  },
+  {
+    "url": "/css/b3658115e242c550ce36.css"
+  },
+  {
+    "url": "/css/ba264fdc8c50ffbe59cb.css"
+  },
+  {
+    "url": "/css/bc14f6bcb4803b93ed30.css"
+  },
+  {
+    "url": "/css/d1e09df4d45ebebf4066.css"
+  },
+  {
+    "url": "/css/f2827cd52af52efa1c8c.css"
+  },
+  {
+    "url": "/images/107880c9b80284e013a767f5657ae5c4.png"
+  },
+  {
+    "url": "/images/23922f42b63b54ab0d43a5ba2871d4c9.png"
+  },
+  {
+    "url": "/images/2f13360b3c585811c666ddcb9fa5fcaf.jpg"
+  },
+  {
+    "url": "/images/32fb14af557598369f3390c7aaa04c79.webp"
+  },
+  {
+    "url": "/images/35cce22958d1792868c4fc9438ccf19e.png"
+  },
+  {
+    "url": "/images/48398b275f85a2e125c27eb9dba402e7.webp"
+  },
+  {
+    "url": "/images/543d7ff6a34cf2fbf2b094a9ae35cee7.png"
+  },
+  {
+    "url": "/images/5ee3a36767902d997905f365565041ed.png"
+  },
+  {
+    "url": "/images/69dea52405c3aad0e969fb69900bf0a0.png"
+  },
+  {
+    "url": "/images/7469a122e0c265f0a88065deedd331a6.jpg"
+  },
+  {
+    "url": "/images/95044a3a9dd1964bba44980e5ebcc749.jpg"
+  },
+  {
+    "url": "/images/9ebde8a259df9506b435f04d04fe27ab.png"
+  },
+  {
+    "url": "/images/a5e02bfc982910f7fcd8dddaa299a23c.jpg"
+  },
+  {
+    "url": "/images/ca078c8cec8d8bf3a5447672b878409c.webp"
+  },
+  {
+    "url": "/images/d25541364d81c3beefd39fd23ce43531.webp"
+  },
+  {
+    "url": "/images/d446f65513553238a4eb4de2326bc3c2.jpg"
+  },
+  {
+    "url": "/images/e2b8677902c8baaa889021c8648d8d4b.png"
+  },
+  {
+    "url": "/images/e7226e02d87a56055616f91de81531de.png"
+  },
+  {
+    "url": "/images/eca67df1dd7e1c5e7a6572137deea725.png"
+  },
+  {
+    "url": "/images/fd396392d98dc725d14146c6f88ef232.webp"
+  },
+  {
+    "url": "/images/ff23eef5a4c4812bf961365acd20aaf2.jpg"
+  },
+  {
+    "url": "/js/0b6fbabf426546b673ba.js"
+  },
+  {
+    "url": "/js/2b4249d05aba060901db.js"
+  },
+  {
+    "url": "/js/456767719c2ad7a9cabd.js"
+  },
+  {
+    "url": "/js/4c5548733f4bec0f19d3.js"
+  },
+  {
+    "url": "/js/6f89bec03111f7d3bcc0.js"
+  },
+  {
+    "url": "/js/81cf936d4ec04f90189f.js"
+  },
+  {
+    "url": "/js/b3658115e242c550ce36.js"
+  },
+  {
+    "url": "/js/b7d9a6984071912909ba.js"
+  },
+  {
+    "url": "/js/ba264fdc8c50ffbe59cb.js"
+  },
+  {
+    "url": "/js/bc14f6bcb4803b93ed30.js"
+  },
+  {
+    "url": "/js/c453f8c0779b6a312ac8.js"
+  },
+  {
+    "url": "/js/ccdfba16e698cee01534.js"
+  },
+  {
+    "url": "/js/d1e09df4d45ebebf4066.js"
+  },
+  {
+    "url": "/js/f2827cd52af52efa1c8c.js"
+  },
+  {
+    "url": "/js/ff650333dc7acbdbf55d.js"
+  }
+]);
---
+++ build/sw.js
@@ -0,0 +1,76 @@
+self.paw__env={"PAW_CACHE":"false","PAW_CONFIG_PATH":"F:\\wamp64\\www\\react-pwa\\prod-pawconfig.json","PAW_ENV":"production","PAW_HOT":"false","PAW_VERBOSE":"false"};importScripts("/precache-manifest.d28913491a8f25d6513c193af019612f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// eslint-disable-next-line
+const serviceWorker = self;
+
+serviceWorker.addEventListener('install', () => {
+  serviceWorker.skipWaiting();
+});
+serviceWorker.addEventListener('activate', () => {
+  serviceWorker.clients.claim();
+});
+
+serviceWorker.workbox.setConfig({
+  debug: serviceWorker.paw__env.PAW_ENV !== 'production',
+});
+
+const getOfflineHtml = () => {
+  const scripts = serviceWorker.paw__offline_assets.filter(a => a.endsWith('.js')).map(js => `<script type="text/javascript" src="${js}" async></script>`).join('');
+  return `<!DOCTYPE html><html><head></head><body><div id="${serviceWorker.paw__injected_variables.clientRootElementId}"></div>${scripts}</body></html>`;
+};
+
+serviceWorker.workbox.routing.registerRoute(
+  new RegExp(`^${serviceWorker.location.origin}/.*__hmm_update.*`),
+  serviceWorker.workbox.strategies.networkOnly(),
+);
+
+const assetsRegExp = /\.(css|js|jpg|png|jpeg|gif|woff|woff2|ttf|eot|ico|mp4|avi)$/;
+
+const networkFirstHandler = serviceWorker.workbox.strategies.networkFirst();
+const cacheFirstHandler = serviceWorker.workbox.strategies.cacheFirst();
+const staleHandler = serviceWorker.workbox.strategies.staleWhileRevalidate();
+
+serviceWorker.workbox.routing.setDefaultHandler(({ event }) => {
+  const { request } = event;
+  const requestMethod = request.method.toUpperCase();
+
+  if (requestMethod !== 'GET') {
+    return fetch(event.request);
+  }
+
+  if (
+    request.url.indexOf(serviceWorker.location.origin) !== -1
+    && assetsRegExp.test(request.url)
+  ) {
+    return cacheFirstHandler.handle({ event });
+  }
+
+  if (
+    request.url.indexOf(serviceWorker.location.origin) === -1
+    && assetsRegExp.test(request.url)
+  ) {
+    return staleHandler.handle({ event });
+  }
+
+  if (
+    request.url.indexOf(serviceWorker.location.origin) !== -1
+    && request.headers.get('accept').indexOf('html') !== -1
+  ) {
+    return networkFirstHandler.handle({ event }).then((response) => {
+      if (!response) {
+        return new Response(
+          getOfflineHtml(),
+          { headers: { 'Content-Type': 'text/html' } },
+        );
+      }
+      return response;
+    });
+  }
+
+  return networkFirstHandler.handle({ event });
+});
+
+// eslint-disable-next-line
+serviceWorker.workbox.precaching.precacheAndRoute(serviceWorker.__precacheManifest);
+
+;self.paw__offline_assets = ["/css/bc14f6bcb4803b93ed30.css","/js/bc14f6bcb4803b93ed30.js","/css/ba264fdc8c50ffbe59cb.css","/js/ba264fdc8c50ffbe59cb.js"];self.paw__injected_variables = {"workboxDebug":true,"port":"3003","host":"0.0.0.0","appRootUrl":"","cdnUrl":"","serverSideRender":true,"serviceWorker":true,"singlePageApplication":false,"asyncCSS":true,"polyfill":"cdn","clientRootElementId":"app","hstsEnabled":true,"hstsmaxAge":31536000,"hstsIncludeSubDomains":true,"hstsPreload":false,"noJS":false,"react":"cdn","resourcesBaseUrl":"/"};
